3c84ad351b24bdeb02c6cf336928ec878879468e
Lisa <lisamariehege@gmail.com>
2019-12-28 04:04:45 +0100

    init

--- addToCart.js
+++ addToCart.js
@@ -16,6 +16,7 @@ if ((!localStorage.getItem('cart'))) {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
+
 let allButtons = document.getElementsByClassName("addBtn");
 for (let i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener('click', function (e) {
